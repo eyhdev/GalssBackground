ddbee8550d2896c449d28d8ea5d553edf1aba826
eyhdev <alaqsa335@gmail.com>
2023-09-21 16:41:00 +0300

    Initial Commit

---
+++ GalssBackground/GalssBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalssBackgroundApp.swift
+//  GalssBackground
+//
+//  Created by eyh.mac on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GalssBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
